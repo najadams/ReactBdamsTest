c16713ee0783f0ff930a25091b6f36bfbf769a41
najadams <najmadams1706@gmail.com>
2023-08-02 19:53:40 +0000

    completed the cart item context code

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,26 +1,28 @@
-import React, { useContext } from 'react'
-import './cart.css'
-import { ShopContext } from '../../context/shop-context'
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const CartItem = (props) => {
-    const {id,productName, price, productImage } = props.data
-    const {cartItems, addToCart, removeFromCart} = useContext(ShopContext)
-    return (
+  const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
 
-    <div className='cartItem'>
-        <img src={productImage} />
-
-        <div className="description">
-            <p><b>{productName}</b></p>
-            <p>${price}</p>
-
-            <div className='countHandler'>
-                <button onClick={addToCart(id)}>+</button>
-                <input value={cartItems[id]} />
-                <button onClick={removeFromCart(id)}>-</button>
-            </div>
-
+  return (
+    <div className="cartItem">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> Price: ${price}</p>
+        <div className="countHandler">
+          <button onClick={() => removeFromCart(id)}> - </button>
+          <input
+            value={cartItems[id]}
+            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+          />
+          <button onClick={() => addToCart(id)}> + </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
